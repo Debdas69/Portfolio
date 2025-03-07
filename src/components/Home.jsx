ae27f9d6d898c1ed0a1da34ccc50d363df3354b0
Debdas69 <deb05838@gmail.com>
2025-03-07 22:31:31 +0530

    Animated Portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react'
+import pdf from '../pdf/resume.pdf'
+import hero from './Data/hero.json'
+import Typed from 'typed.js'
+
+
+const Home = () => {
+      const typeRef = useRef(null)
+
+      useEffect(() => {
+       
+        const options = {
+          strings:["Welcome to my profile", "My name is Debdas Patra","I am frontent devoloper","PHP devoloper (Laravel)"],
+          typeSpeed:50,
+          backSpeed:50,
+          loop:true
+        }
+        const typed = new Typed(typeRef.current, options)
+      
+        return () => {
+         typed.destroy()
+        }
+      }, [])
+      
+
+  return (
+    <>
+    <div className="container home">
+    <div className="left "  data-aos="fade-up-right"
+      data-aos-duration="1000">
+      <h1 ref={typeRef}></h1>
+
+      <a href={pdf} download="resume.pdf" className="btn btn-outline-warning my-3">Download Resume</a>
+    </div>
+    <div className="right">
+      <div className="img"  data-aos="fade-up-left"
+      data-aos-duration="1000">
+        <img src={`/public/${hero.imgSrc}`} alt="hero" />
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
